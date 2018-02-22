a8f801d8a2a1e727d0ec6ccf7ace74cf9fadd0d3
tperezrodriguez <tperezrodriguez@victoria5.danielcastelao.org>
2018-02-22 12:24:08 +0100

    Añadida la librería y comprobada

--- src/cod_programalibrerias/COD_ProgramaLibrerias.java
+++ src/cod_programalibrerias/COD_ProgramaLibrerias.java
@@ -1,10 +1,15 @@
 
 package cod_programalibrerias;
 
+import COD_Libreria.Libreria;
+
 public class COD_ProgramaLibrerias {
 
     public static void main(String[] args) {
-       
+        System.out.println("¿Funcionará la librería de Laura?");
+        Libreria.mostrarMensaje("¡Pues claro que funciona!");
+        Libreria.pedirDatoDouble("¿Me dirías un número?");
+        
     }
     
 }
